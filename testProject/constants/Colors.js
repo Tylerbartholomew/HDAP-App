f2b808b4483c8459cada0b3b2b599dd3384d660e
Rsaperstein <33642090+Rsaperstein@users.noreply.github.com>
2019-10-25 14:22:26 -0400

    Update Colors.js

--- testProject/constants/Colors.js
+++ testProject/constants/Colors.js
@@ -1,12 +1,12 @@
-const primary = '#6d76c0';
+const primary = '#6d76c0';      // Medium Purple: RGB(109, 118, 192), HSL(233, 40%, 59%)
 
 export default {
   primary: primary,
-  primaryDark: '#4a5082',
+  primaryDark: '#4a5082',       // Dark Purple: RGB(74, 80, 130), HSL(234, 27%, 40%)
   errorBackground: 'red',
-  errorText: '#fff',
-  warningBackground: '#EAEB5E',
-  warningText: '#666804',
+  errorText: '#fff',            // White
+  warningBackground: '#EAEB5E', // Yellow: RGB(234, 235, 94), HSL(60, 78%, 65%)
+  warningText: '#666804',       // Dark Green: RGB(102, 104, 4), HSL(61, 93%, 21%))
   noticeBackground: primary,
-  noticeText: '#fff',
+  noticeText: '#fff',           // White
 };
